1cc7206aa2606c0580db6f78b35a8717f3ef625d
thomas <132210659+TDev04@users.noreply.github.com>
2024-04-11 12:33:45 +0200

    Update drv.h

--- src/drv.h
+++ src/drv.h
@@ -49,19 +49,3 @@ void io_wait(void)
 {
     outb(0x80, 0);
 }
-
-void clear(int sector) {
-    outb(0x1F2, 0x00);       // mov	dx, 1F2h; mov	al, 2 ; sector number; out	dx,al
-    outb(0x1F3, sector + 1); // inc	dx; out	dx,al
-    outb(0x1F4, 0x00);       // inc	dx; xor	ax,ax; out	dx,al
-    outb(0x1F5, 0x00);       // inc	dx; out	dx,al
-    outb(0x1F6, 0xA0);       // mov	al, 10100000b; inc	dx; out	dx,al
-    outb(0x1F7, 0x30);       // inc	dx; mov	al,30h; out	dx,al
-    uint16_t *buffer = (uint16_t *)0xC000; // lea	si, [0c000h]
-    uint16_t port = 0x1F0;   // mov	dx, 1F0h
-    int i;
-    for (i = 0; i < 512; i++) {
-        outw(port, buffer[i]); // mov	cx, 512; rep	outsw
-    }
-    return;                  // ret
-}
